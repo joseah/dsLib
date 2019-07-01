f44756fe5bdb2ba7ccf3e9685115bd64cc12b47f
joseah <alquicirajose@gmail.com>
2019-07-01 21:18:38 +1000

    adds memory usage feature to init() and done() functions

--- R/echo.R
+++ R/echo.R
@@ -1,18 +1,22 @@
 #' @title Prints formatted message
 #' @author Jose Alquicira-Hernandez
 #' @param string Message to print
-#' @param lb If `TRUE`, a break line is included at the start of the string
+#' @param start A string symbol
+#' @param ellipsis If TRUE, adds ellipsis to message
+#' @param mem Track memory usage?
 #' @importFrom  tictoc tic
+#' @importFrom pryr mem_used
 #' @export
 #' @examples
 #'
 #' init("Analyzing data")
 #'
 
-init <- function(string, lb = TRUE, ellipsis = TRUE, tic = TRUE) {
+init <- function(string = NULL, start = "==>", ellipsis = TRUE, mem = TRUE) {
 
-  if(!lb %in% c(0, 1, 2)){
-    stop("`lb` must be 2, 1, or 0")
+  if(is.null(string)){
+    string <- ""
+    ellipsis <- FALSE
   }
 
   if(ellipsis){
@@ -21,46 +25,70 @@ init <- function(string, lb = TRUE, ellipsis = TRUE, tic = TRUE) {
     ellipsis <- ""
   }
 
-
   final <- paste0(" ", ellipsis ,"\n")
+  mem <- capture.output(print(mem_used()))
+  mem <- paste0(">> Current mem use = ", mem)
+  final <- paste0(final, mem)
 
-  if(lb){
-    start <- "\n"
-  }else{
-    start <- ""
-  }
-
-  cat(start, "==>", paste0(format(Sys.time(), "(%d/%b/%Y %X): "), string, final), sep = "")
-  if(tic) tic()
+  cat(start, paste0(format(Sys.time(), "(%d/%b/%Y %X): "), string, final), sep = "")
+  tic()
 
 }
 
 
 #' @title Prints "DONE!" message
 #' @author Jose Alquicira-Hernandez
-#' @param toc If TRUE and `tic()` was used before, displays elapsed time
 #' @importFrom tictoc toc
+#' @param start A string symbol
 #' @export
 #' @examples
 #' done()
 #'
 
-done <- function(toc = TRUE){
-  if(toc){
-    x <- toc(log = TRUE, quiet = TRUE)
+done <- function(start = "==>"){
 
+  mem <- capture.output(print(mem_used()))
+  mem <- paste0(">> Final mem use = ", mem)
+
+  x <- toc(log = TRUE, quiet = TRUE)
+
+  if(!is.null(x)){
     timeDiff <- x$toc - x$tic
-
-    if(timeDiff > 60){
-      timeDiff <- timeDiff/60
-      timeUnit <- "mins"
-    }else{
-      timeUnit <- "secs"
-    }
-    timeDiff <- round(timeDiff, digits = 3)
-    string <- paste("DONE!", "\n>>> Elapsed time:", timeDiff, timeUnit)
+    time <- formatTime(timeDiff)
+    timeDiff <- round(time$value, digits = 3)
+    timeUnit <- time$unit
+    string <- paste(">>> Elapsed time:", timeDiff, timeUnit, "\n", start, " DONE!")
   }else{
-    string <- "DONE!"
+    string <- paste0(start, " DONE!")
   }
-  init(string, ellipsis = FALSE, lb = FALSE, tic = FALSE)
+
+
+  string <- paste0(mem, "\n", string)
+  cat(string)
+}
+
+#' @title Formats time for seconds, minutes and hours
+#' @author Jose Alquicira-Hernandez
+#' @param x Numeric value representing seconds
+#' @return A list with the format-numeric value and its time unit
+#' @export
+#' @examples
+#' formatTime(60)
+#' formatTime(60 * 60)
+#'
+
+
+formatTime <- function(x){
+
+  if(x >= 3600){
+    x <- x/60/60
+    unit <- "hrs"
+  }else if(x >= 60){
+    x <- x/60
+    unit <- "mins"
+  }else{
+    unit <- "secs"
+  }
+
+  list(value = x, unit = unit)
 }
