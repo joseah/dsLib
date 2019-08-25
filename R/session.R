b90dcde51fc5593183115593a17876a7426f18d4
joseah <alquicirajose@gmail.com>
2019-08-25 23:59:02 +1000

    prints commit information if a repository is available

--- R/session.R
+++ R/session.R
@@ -1,9 +1,23 @@
 #' @title Prints R session info
 #' @author Jose Alquicira-Hernandez
-#' @export
+#' @importFrom here here
+#' @importFrom git2r revparse_single
+#' @importFrom git2r repository
 #' @importFrom devtools session_info
+#' @export
 
 printSession  <- function() {
+  repo <- tryCatch(revparse_single(repository(here()),"HEAD"),
+                   error = function(e) {
+                     NULL
+                   })
+  if(!is.null(repo)){
+    cat("- Git details:", sep = "\n")
+    cat("Commit:", repo$sha, "\n")
+    cat("Message:", repo$summary, "\n")
+    print(repo$author)
+    cat("\n")
+  }
   options(width = 90)
   session_info()
 }
