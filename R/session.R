423d8cea10307ed83a14ecd20e68d53435175618
joseah <alquicirajose@gmail.com>
2019-10-16 02:17:58 +1100

    writes session info to file

--- R/session.R
+++ R/session.R
@@ -21,3 +21,37 @@ printSession  <- function() {
   options(width = 90)
   session_info()
 }
+
+
+printSession  <- function(path = NULL) {
+  output <- ""
+  repo <- tryCatch(revparse_single(repository(here()),"HEAD"),
+                   error = function(e) {
+                     NULL
+                   })
+  if(!is.null(repo)){
+    output <- "- Git details:\n"
+    output <- paste0(output, "Commit: ", repo$sha, "\n")
+    output <- paste0(output, "Message: ", repo$summary, "\n")
+    author <- capture.output(print(repo$author))
+    output <- paste0(output, paste0(author, collapse = "\n"), "\n")
+  }
+
+  output <- paste0(output, paste0(session_info(), collapse = "\n"))
+
+  if(is.null(path)){
+    path <- "session.info"
+  }else{
+    path <- here(path, "session.info")
+  }
+
+  # Write file
+  cat(output, file = path)
+
+  # Print output
+  width <- options()$width
+  options(width = 90)
+  cat(output)
+  options()$width <- width
+
+}
